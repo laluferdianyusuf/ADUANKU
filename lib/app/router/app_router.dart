22bb62ad349d37d96127e4e154672259ff7ed8e6
laluferdianyusuf <lferdianyusuf@gmail.com>
2026-09-17 12:29:46 +0800

    change color when tab is active

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,5 +1,5 @@
+import 'package:aduanku/app/theme/app_colors.dart';
 import 'package:aduanku/features/cases/pages/case_list_page.dart';
-import 'package:aduanku/features/cases/pages/create_case_page.dart';
 import 'package:aduanku/features/home/pages/home_page.dart';
 import 'package:aduanku/features/information/pages/information_page.dart';
 import 'package:aduanku/features/profile/pages/profile_page.dart';
@@ -39,18 +39,6 @@ final GoRouter appRouter = GoRouter(
               name: 'cases',
               builder: (context, state) {
                 return const CaseListPage();
-              },
-            ),
-          ],
-        ),
-
-        StatefulShellBranch(
-          routes: [
-            GoRoute(
-              path: '/report',
-              name: 'report',
-              builder: (context, state) {
-                return const CreateCasePage();
               },
             ),
           ],
@@ -104,36 +92,54 @@ class _MainShellState extends State<MainShell> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: widget.navigationShell,
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: widget.navigationShell.currentIndex,
-        onDestinationSelected: widget._onDestinationSelected,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Iconsax.home),
-            selectedIcon: Icon(Iconsax.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.document),
-            selectedIcon: Icon(Iconsax.document),
-            label: 'Cases',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.add),
-            selectedIcon: Icon(Iconsax.add),
-            label: 'Report',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.info_circle),
-            selectedIcon: Icon(Iconsax.info_circle),
-            label: 'Information',
-          ),
-          NavigationDestination(
-            icon: Icon(Iconsax.profile_circle),
-            selectedIcon: Icon(Iconsax.profile_circle),
-            label: 'Profile',
-          ),
-        ],
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: AppColors.primary.withOpacity(0.15),
+          labelTextStyle: WidgetStateProperty.resolveWith<TextStyle>((states) {
+            if (states.contains(WidgetState.selected)) {
+              return const TextStyle(
+                color: AppColors.primary,
+                fontWeight: FontWeight.w600,
+              );
+            }
+
+            return const TextStyle(color: Colors.grey);
+          }),
+          iconTheme: WidgetStateProperty.resolveWith<IconThemeData>((states) {
+            if (states.contains(WidgetState.selected)) {
+              return const IconThemeData(color: AppColors.primary);
+            }
+
+            return const IconThemeData(color: Colors.grey);
+          }),
+        ),
+        child: NavigationBar(
+          selectedIndex: widget.navigationShell.currentIndex,
+          onDestinationSelected: widget._onDestinationSelected,
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Iconsax.home),
+              selectedIcon: Icon(Iconsax.home),
+              label: 'Home',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.document),
+              selectedIcon: Icon(Iconsax.document),
+              label: 'Cases',
+            ),
+
+            NavigationDestination(
+              icon: Icon(Iconsax.info_circle),
+              selectedIcon: Icon(Iconsax.info_circle),
+              label: 'Information',
+            ),
+            NavigationDestination(
+              icon: Icon(Iconsax.profile_circle),
+              selectedIcon: Icon(Iconsax.profile_circle),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
